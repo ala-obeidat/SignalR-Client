e34363e6e4ae25b6955d5268f92fc6ff6cc6cae9
Ala Obeidat <ala-work@hotmail.com>
2021-07-06 16:13:21 +0300

    First publishing

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace SiganR.Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.txtMethod = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtPayload = new System.Windows.Forms.RichTextBox();
+            this.txtResult = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtArgument = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.chbInteger = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(115, 49);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(547, 27);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // txtMethod
+            // 
+            this.txtMethod.Location = new System.Drawing.Point(115, 120);
+            this.txtMethod.Name = "txtMethod";
+            this.txtMethod.Size = new System.Drawing.Size(180, 27);
+            this.txtMethod.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Method";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(572, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Payload";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnConnect.Location = new System.Drawing.Point(677, 35);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(153, 45);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSend.Location = new System.Drawing.Point(115, 207);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(439, 69);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtPayload
+            // 
+            this.txtPayload.Location = new System.Drawing.Point(572, 120);
+            this.txtPayload.Name = "txtPayload";
+            this.txtPayload.Size = new System.Drawing.Size(258, 156);
+            this.txtPayload.TabIndex = 7;
+            this.txtPayload.Text = "";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(119, 320);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(711, 262);
+            this.txtResult.TabIndex = 8;
+            this.txtResult.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(119, 297);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Result";
+            // 
+            // txtArgument
+            // 
+            this.txtArgument.Location = new System.Drawing.Point(301, 120);
+            this.txtArgument.Name = "txtArgument";
+            this.txtArgument.PlaceholderText = "Sperated by ,";
+            this.txtArgument.Size = new System.Drawing.Size(253, 27);
+            this.txtArgument.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(301, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Argument";
+            // 
+            // chbInteger
+            // 
+            this.chbInteger.AutoSize = true;
+            this.chbInteger.Location = new System.Drawing.Point(301, 154);
+            this.chbInteger.Name = "chbInteger";
+            this.chbInteger.Size = new System.Drawing.Size(75, 24);
+            this.chbInteger.TabIndex = 12;
+            this.chbInteger.Text = "Integer";
+            this.chbInteger.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1040, 594);
+            this.Controls.Add(this.chbInteger);
+            this.Controls.Add(this.txtArgument);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtPayload);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMethod);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "SignalR Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.TextBox txtMethod;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.RichTextBox txtPayload;
+        private System.Windows.Forms.RichTextBox txtResult;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtArgument;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox chbInteger;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.Json;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace SiganR.Client
+{
+    public partial class Form1 : Form
+    {
+        private HubConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+            btnSend.Enabled = false;
+        }
+
+        private async void btnConnect_Click(object sender, EventArgs e)
+        {
+            await OpenConnection();
+        }
+        private void EstablishConnection()
+        {
+            if (string.IsNullOrEmpty(txtUrl.Text))
+            {
+                ShowAlert("Please Fill URL to connect");
+                return;
+            }
+            if (connection == null)
+            {
+                connection = new HubConnectionBuilder()
+                    .WithUrl(txtUrl.Text).WithAutomaticReconnect()
+                    .Build();
+
+                connection.Closed += async (error) =>
+                {
+                    AppendResult("Connection Closed");
+                    AppendResult(error);
+                    await Task.Delay(new Random().Next(0, 5) * 1000);
+                    await OpenConnection();
+                };
+            }
+        }
+        private void ShowAlert(string message)
+        {
+            MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        private async Task OpenConnection()
+        {
+            try
+            {
+                EstablishConnection();
+                await connection.StartAsync();
+                AppendResult("Connection started");
+                btnSend.Enabled = true;
+                btnConnect.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                AppendResult(ex);
+            }
+        }
+        private void AppendResult(Exception ex)
+        {
+
+            txtResult.Text += $"\nError: {ex.Message}";
+        }
+        private void AppendResult(string message)
+        {
+
+            txtResult.Text += $"\n{message}";
+        }
+
+        private async void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connection.State != HubConnectionState.Connected)
+                {
+                    AppendResult("Reconnecting to Hup");
+                    await OpenConnection();
+                }
+                if (string.IsNullOrEmpty(txtMethod.Text))
+                {
+                    ShowAlert("Please Fill Method to send");
+                    return;
+                }
+                if (string.IsNullOrEmpty(txtPayload.Text) || !string.IsNullOrEmpty(txtArgument.Text))
+                {
+                    object result = null;
+                    if (string.IsNullOrEmpty(txtArgument.Text))
+                    {
+                        result = await connection.InvokeAsync<object>(txtMethod.Text);
+                    }
+                    else
+                    {
+                        if (chbInteger.Checked)
+                        {
+                            result = await SendMethod(txtArgument.Text.Split(',').Select(x=>Convert.ToInt32(x)).ToArray());
+                        }
+                        else
+                        {
+                            result = await SendMethod(txtArgument.Text.Split(','));
+                        }
+                        
+                    }
+                    if (result != null)
+                    {
+                        AppendResult($"Response: {result.ToString()}");
+                    }
+                }
+                else
+                {
+                    var model = JsonSerializer.Deserialize<object>(txtPayload.Text);
+                    await connection.InvokeAsync(txtMethod.Text, model);
+                }
+                AppendResult($"{txtMethod.Text} Sended successfully");
+            }
+            catch (Exception ex)
+            {
+                AppendResult(ex);
+            }
+        }
+
+        private async Task<object> SendMethod(string[] args)
+        {
+            switch (args.Length)
+            {
+                case 1:
+                    return await connection.InvokeAsync<object>(txtMethod.Text, args[0]);
+                case 2:
+                    return await connection.InvokeAsync<object>(txtMethod.Text, args[0], args[1]);
+                case 3:
+                    return await connection.InvokeAsync<object>(txtMethod.Text, args[0], args[1], args[2]);
+                case 4:
+                    return await connection.InvokeAsync<object>(txtMethod.Text, args[0], args[1], args[2], args[3]);
+                case 5:
+                    return await connection.InvokeAsync<object>(txtMethod.Text, args[0], args[1], args[2], args[3], args[4]);
+                case 6:
+                    return await connection.InvokeAsync<object>(txtMethod.Text, args[0], args[1], args[2], args[3], args[4], args[5]);
+            }
+
+            return null;
+        }
+        private async Task<object> SendMethod(int[] args)
+        {
+            switch (args.Length)
+            {
+                case 1:
+                    return await connection.InvokeAsync<object>(txtMethod.Text, args[0]);
+                case 2:
+                    return await connection.InvokeAsync<object>(txtMethod.Text, args[0], args[1]);
+                case 3:
+                    return await connection.InvokeAsync<object>(txtMethod.Text, args[0], args[1], args[2]);
+                case 4:
+                    return await connection.InvokeAsync<object>(txtMethod.Text, args[0], args[1], args[2], args[3]);
+                case 5:
+                    return await connection.InvokeAsync<object>(txtMethod.Text, args[0], args[1], args[2], args[3], args[4]);
+                case 6:
+                    return await connection.InvokeAsync<object>(txtMethod.Text, args[0], args[1], args[2], args[3], args[4], args[5]);
+            }
+
+            return null;
+        }
+    }
+}
